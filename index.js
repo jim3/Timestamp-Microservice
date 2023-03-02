0213e282ecf90b2ab88ecb015b765ec8373cafbb
jim3 <j3.casey@protonmail.com>
2023-03-02 18:32:51 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+    res.json({ greeting: "hello API" });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});

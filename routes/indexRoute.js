02e0073e4827ee61866b5ecd513df9e32d63df6a
jim3 <j3.casey@protonmail.com>
2023-03-03 16:40:34 -0500

    Update indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -6,19 +6,59 @@ router.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-// test route
-router.get("/api/hello", function (req, res) {
-    res.json({ greeting: "hello API" });
+router.get("/", function (req, res) {
+    res.render("index");
 });
 
-// api endpoint for the timestamp microservice
-router.get("/api/:date", function (req, res) {
-    let isoStr = req.params.date;
-    const unixTimeStamp = timeConverter(isoStr);
-    const utcTime = timeConverter(isoStr);
+// route if no timestamp is provided
+router.get("/api", function (req, res) {
+    const unixTimeStamp = Date.now(); // get current unix timestamp
+    const utcTime = new Date(unixTimeStamp).toUTCString(); // convert unix timestamp to utc time
     res.json({
-        unix: unixTimeStamp.unix,
-        utc: utcTime.utc,
+        unix: unixTimeStamp,
+        utc: utcTime,
+    });
+});
+
+// route if timestamp is provided
+router.get("/api/:timestamp", function (req, res) {
+    const { timestamp } = req.params;
+    let unixTimeStamp;
+    let utcTime;
+
+    if (/^\d+$/.test(timestamp)) {
+        unixTimeStamp = parseInt(timestamp);
+        utcTime = new Date(unixTimeStamp).toUTCString();
+    } else {
+        const result = timeConverter(timestamp);
+        if (result.error) {
+            res.status(400).json({ error: result.error });
+            return;
+        }
+        unixTimeStamp = result.unix;
+        utcTime = result.utc;
+    }
+
+    res.json({
+        unix: unixTimeStamp,
+        utc: utcTime,
+    });
+});
+
+// route if date and timestamp is provided
+router.get("/api/:timestamp/:date", function (req, res) {
+    const { timestamp, date } = req.params;
+    const isoStr = `${date} ${timestamp}`;
+    const result = timeConverter(isoStr);
+
+    if (result.error) {
+        res.status(400).json({ error: result.error });
+        return;
+    }
+
+    res.json({
+        unix: result.unix,
+        utc: result.utc,
     });
 });
 

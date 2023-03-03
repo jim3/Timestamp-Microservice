d5de4647fe55a7a88b2e03c36073ac19df20b4e8
jim3 <j3.casey@protonmail.com>
2023-03-03 16:41:23 -0500

    Update timeConverter.js

--- services/timeConverter.js
+++ services/timeConverter.js
@@ -1,17 +1,18 @@
 // (((Time·Converter·Service)))
 const timeConverter = (isoStr) => {
-    if (!isoStr) {
-        isoStr = new Date().toISOString();
-    }
-    if (Number.isInteger(isoStr)) {
-        isoStr = new Date(isoStr).toISOString();
-    }
+    // if given a date string, convert it to a date
     if (typeof isoStr === "string") {
         isoStr = new Date(isoStr);
     }
+
+    // if the date is not provided, use the current date
+    if (!isoStr) {
+        isoStr = new Date().toISOString();
+    }
+
     // if the date is invalid, return an error
     if (isoStr.toString() === "Invalid Date") {
-        res.json({ error: "Invalid Date" });
+        return { error: "Invalid Date" };
     }
     const unixTimeStamp = new Date(isoStr).getTime();
     const utcTime = new Date(isoStr).toUTCString();
